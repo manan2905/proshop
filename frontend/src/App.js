0b3e518aebad7a88ea9da4d2a29f2b897a55b7b5
manan2905 <anand.manan29@gmail.com>
2023-07-29 01:31:21 +0530

    Product Component & HomeScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 import { Component } from 'react';
 import { Container } from 'react-bootstrap';
 
@@ -11,7 +12,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome to Proshop!</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
