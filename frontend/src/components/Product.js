988425344a43257da210f533b8c8c9a973490d94
manan2905 <anand.manan29@gmail.com>
2023-07-31 17:19:05 +0530

    Product Details Page & Rating Component

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,6 @@
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Rating from "./Rating";
 
 const Product = ({product}) => {    
   return (
@@ -8,11 +9,12 @@ const Product = ({product}) => {
             <Card.Img src = {product.image} variant='top' />
         </Link>
         <Card.Body>
-            <a href={`/product/${product._id}`}>
-                <Card.Title as="div">
+            <Link to={`/product/${product._id}`}>
+                <Card.Title as="div" className="product-title">
                     <strong>{product.name}</strong>
                 </Card.Title>
-            </a>
+            </Link>
+            <Card.Text as="div"><Rating value={product.rating} text={`${product.numReviews} reviews`}/></Card.Text>
             <Card.Text as="h3">${product.price}</Card.Text>
         </Card.Body>
     </Card>
