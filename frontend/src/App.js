12166a719543bc1ead548be564d39d76a25242a4
manan2905 <anand.manan29@gmail.com>
2023-07-31 13:51:08 +0530

    Adding React Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import HomeScreen from "./screens/HomeScreen";
+import { Outlet } from 'react-router-dom';
 import { Component } from 'react';
 import { Container } from 'react-bootstrap';
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 
 const App = () => {
@@ -12,7 +13,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Outlet />
         </Container>
       </main>
       <Footer />
